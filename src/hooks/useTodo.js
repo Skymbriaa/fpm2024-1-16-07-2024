694bebeea011f912ed3d9ce681d32f251ec25e9c
Skymbriaa <pozilaaskymbria@gmail.com>
2024-07-16 11:04:00 +0300

    Add task

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -1,10 +1,19 @@
 import { useState } from "react";
+import { v4 as uuidv4} from 'uuid';
 
 
 const useTodo = (initialTasks) => {
     const [tasks, setTasks] = useState([...initialTasks]);
+    const addTask = (values) => {
+        const newTask = {
+            id: uuidv4(),
+            content: values.content,
+            iaDone: false
+        };
+        setTasks([...tasks, newTask]);
+    };
 
-    return {tasks, setTasks}
+    return {tasks, setTasks , addTask}
 }
 
 export default useTodo;
